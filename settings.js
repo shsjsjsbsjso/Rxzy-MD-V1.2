28bc3249aea501ff110b422fb53c004919ce5115
shsjsjsbsjso <148639262+shsjsjsbsjso@users.noreply.github.com>
2023-10-24 10:01:41 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,65 @@
+/*
+   Created By ArxzyDev
+   My Contact wa.me/6289513081052
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+/* ~~~~~~~~~ WEB API ~~~~~~~~~ */
+global.lol = '' // https://api.lolhuman.xyz
+global.xzn = '' // https://xnz.wtf
+/* ~~~~~~~~~ SETTINGS OWNER ~~~~~~~~~ */
+global.numberowner = '6281299609670' // Owner Utama
+global.owner = ['6281299609670', '6281299609670'] // Owner Lainnya
+global.namaowner = 'XdimsDev お-ぎ' // Nama Owner
+global.premium = ["6281299609670"] // Premium User
+global.nobot = '6287887745354'
+/* ~~~~~~~~~ SETTINGS BOT ~~~~~~~~~ */
+global.namabot = 'XdimsBOT-MD' // NickBot
+global.typemenu = 'v2' // 'v1' > 'v2' > 'v3' > 'v4'
+global.typereply = 'v1' // 'v1' > 'v2'
+global.autoread = false // ReadChat
+global.autobio = false // AutoBio
+global.autoblok212 = true // AutoBlock Nomer +212
+global.onlyindo = false  // AutoBlock Selain Nomer Indo
+global.packname = 'Created By' // Watermark Sticker
+global.author = 'XdimsDev ダ' // Watermark Sticker
+/* ~~~~~~~~~ MESSAGES ~~~~~~~~~ */
+global.mess = {
+    done: 'Done ✅',
+    prem: 'Feature Only For User _*PREMIUM*_',
+    admin: 'Feature Only for _*Admin Group*_',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: 'Feature Only for _*owner*_',
+    group: 'Feature Only for _*Group Chat*_',
+    private: 'Feature Only for _*Private Chat*_',
+    wait: 'Wait a Moment, for Process',    
+    error: '_*Sorry Features Error*_',
+}
+/* ~~~~~~~~~ THUMBNAIL ~~~~~~~~~ */
+global.thumb = fs.readFileSync('./media/quoted.jpg')
+global.menu = fs.readFileSync('./media/menu.jpg')
+/* ~~~~~~~~~ EDITS LINK ~~~~~~~~~ */
+global.link = 'https://chat.whatsapp.com/F8SPUDO5jotGjMv4O89x3B'
+/* ~~~~~~~~~ END SYSTEM ~~~~~~~~~ */
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
